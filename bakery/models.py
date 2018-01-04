fc19bec8f40bfba9c0fad5c9bbd8a4cea57b6dba
Black-Starlight <Black-Starlight@users.noreply.github.com>
2018-01-04 15:49:43 +0100

    Update models.py

--- bakery/models.py
+++ bakery/models.py
@@ -64,8 +64,5 @@ class NewUsers(models.Model):
    email = models.CharField(max_length=200)
    password = models.CharField(max_length=50)
 
-   def approve(self):
-        self.save()
-
    def __str__(self):
         return self.author
