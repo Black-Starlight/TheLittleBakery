c458351f35592425a59a06a0eaedb47700af210e
Black-Starlight <Black-Starlight@users.noreply.github.com>
2018-01-04 15:30:26 +0100

    Update models.py
    New model added

--- bakery/models.py
+++ bakery/models.py
@@ -57,3 +57,16 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.text
+
+    
+class NewUsers(models.Model):
+   author = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.Charfield(max_length=50, min_length=6)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+return self.author
