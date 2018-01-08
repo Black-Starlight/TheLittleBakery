19963f0a891a53d567770efa79bf5ba40156e7da
Black-Starlight <Black-Starlight@users.noreply.github.com>
2018-01-08 11:24:21 +0100

    UserForm added

--- bakery/forms.py
+++ bakery/forms.py
@@ -1,10 +1,17 @@
+from django.contrib.auth.models import User
 from django import forms
 from .models import Comment
 
-
 class CommentForm(forms.ModelForm):
-    #favorite_fruit= forms.CharField(label='What is your favorite fruit?', widget=forms.RadioSelect(choices=FRUIT_CHOICES))
-
+    
     class Meta:
         model = Comment
         fields = ('name', 'rate', 'text')
+        
+class UserForm(forms.ModelForm):
+    password = forms.Charfield(widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
