851da7840a19fa168c819d5102ffd41d441a96ec
Black-Starlight <Black-Starlight@users.noreply.github.com>
2018-01-11 15:49:59 +0100

    Update views.py

--- bakery/views.py
+++ bakery/views.py
@@ -18,39 +18,6 @@ class recipeList(APIView):
     def post(self):
         pass
 
- class registration_form(View):
-    form_class = UserForm
-    template_name = 'bakery/registration_form.html'
-    
-    # display blank form
-    def get(self, request):
-        form = selfform_class(None)
-        return render(request, self.template_name, {"form": form})
-    
-    def post(self, request):
-        form = self.form_class(request.POST)
-        
-        if form.is_valid():
-            
-            user = form.save(commit=False)
-            
-            #cleaned/normalized data
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user.set_password(password)
-            user.save()
-            
-            # returs User objects if credentials are correct
-            user = authenticate(username=username, password=password)
-            
-            if user is not none:
-                
-                if user.is_active:
-                    
-                    login(request, user)
-                    return redirect('bakery:index')
-                    
-        return reder(request, selftemplate_name, {'form': form})
     
 def add_comment_to_recipe(request, pk):
     recipe = get_object_or_404(Recipes, pk=pk)
