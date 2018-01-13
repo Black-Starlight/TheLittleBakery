8d2c0b5619a4a8a122c277ecfd89ce6e5c2fa2d0
Black-Starlight <Black-Starlight@users.noreply.github.com>
2018-01-13 16:31:49 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['127.0.0.1', 'starpower.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
