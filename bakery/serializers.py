36a0ae3d3d4dd26dcf013b49cf38e57a2efe24f8
Marjac <m.jacobs@student.tue.nl>
2018-01-14 15:44:43 +0100

    Update serializers.py

--- bakery/serializers.py
+++ bakery/serializers.py
@@ -4,4 +4,4 @@ from .models import Recipes
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipes
-        fields = ('pk','imageDir','title', 'serve', 'prepTime', 'cookTime', 'kcal', 'fat', 'carbs', 'sugar', 'protein', 'salt', 'ingredients', 'recipe')
+        fields = ('pk','imageDir','caption','title', 'serve', 'prepTime', 'cookTime', 'kcal', 'fat', 'carbs', 'sugar', 'protein', 'salt', 'ingredients', 'recipe')
