3e594f564869ca24c8435d4c1929bea60820f92e
Marjac <m.jacobs@student.tue.nl>
2018-01-15 13:23:58 +0100

    Update serializers.py

--- bakery/serializers.py
+++ bakery/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Recipes
+from .models import Recipes, Profile
 
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipes
         fields = ('pk','imageDir','caption','title', 'serve', 'prepTime', 'cookTime', 'kcal', 'fat', 'carbs', 'sugar', 'protein', 'salt', 'ingredients', 'recipe')
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user','bio', 'location', 'birth_date', 'favs', 'made','friends')
