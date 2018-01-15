6d4b44b7842624dfd77a79dd5d701e77c03cc18c
renate1997 <renatelemmens1997@gmail.com>
2018-01-15 15:18:22 +0100

    Update views.py

--- bakery/views.py
+++ bakery/views.py
@@ -94,24 +94,22 @@ class UserFormView(View):
 '''
 
 def comment(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Comments.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'bakery/comment.html', {'posts': posts})	
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'bakery/post_detail.html', {'post': post})
 
 def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = CommentForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
-            post.save()
+            comment = form.save(commit=False)
+            comment.commenter = request.user
+            comment.published_date = timezone.now()
+	    comment.profile = profile
+            comment.save()
             return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm()
+        form = CommentForm()
     return render(request, 'eetgelegenheden/post_edit.html', {'form': form})
 
 
