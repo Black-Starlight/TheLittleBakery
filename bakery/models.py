71900ebfe18efb5cd360f1db12a67088ea0b777a
Black-Starlight <Black-Starlight@users.noreply.github.com>
2018-01-15 11:52:27 +0100

    Update models.py

--- bakery/models.py
+++ bakery/models.py
@@ -40,6 +40,8 @@ class Recipes(models.Model):
         return self.title
 
 class Comment(models.Model):
+    commenter = models.ForeignKey(User, related_name='commenter')
+    recipe = models.ForeignKey(Recipe, related_name='comments')
     content = models.TextField(default='empty')
     date = models.DateTimeField(default=timezone.now)
     path = models.CharField(validators=[validate_comma_separated_integer_list], blank=True, editable=False, max_length=50)
