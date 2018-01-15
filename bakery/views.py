e7293aac3bb08b64f08fa185b517d7765ad5f890
renate1997 <renatelemmens1997@gmail.com>
2018-01-15 16:32:51 +0100

    Update views.py

--- bakery/views.py
+++ bakery/views.py
@@ -108,6 +108,18 @@ def add_recipe(request):
     form = addRecipeForm()
     return render(request, 'bakery/add_recipe.html', {'form': form})
 
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
 
 
 def index(request):
