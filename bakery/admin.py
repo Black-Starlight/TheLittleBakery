a4cdc7337c1675b5880af69de0fb59d099a9f111
Black-Starlight <Black-Starlight@users.noreply.github.com>
2018-01-15 16:40:13 +0100

    Update admin.py

--- bakery/admin.py
+++ bakery/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Recipes, Comment, Profile, ProfileComments
+from .models import Recipes, Comments, Profile, ProfileComments
 
 
 admin.site.register(Recipes)
-admin.site.register(Comment)
+admin.site.register(Comments)
 admin.site.register(Profile)
 admin.site.register(ProfileComments)
